ee69af3608c01132daa104a20e1cd608cb0d281a
“Eugene Bercowicz” <eugene.bercowicz@wpengine.com>
2021-03-17 11:17:58 +0100

    changes related to authorization process

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # README
 
-
 ### Basic commands needed: 
+
+---
+* bundle install
+* rake db:create
+* rake db:migrate
+* rails s
 * rails db:create 
 * ./bin/webpack-dev-server
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
-  root 'pages#index'
+  resources :users, only: [:new, :create]
+  get 'login', to: 'sessions#new'
+  get 'logout', to: 'sessions#logout'
+  post 'login', to: 'sessions#create'
+  get 'welcome', to: 'sessions#welcome'
+  get 'authorized', to: 'sessions#page_requires_login'
 
-  match '*path', to: 'pages#index', via: :all
+  # root 'pages#index'
+
+  # match '*path', to: 'pages#index', via: :all
+  # get 'sessions/new'
+  # get 'sessions/create'
+  # get 'sessions/login'
+  # get 'sessions/welcome'
+  # get 'users/new'
+  # get 'users/create'
 end
